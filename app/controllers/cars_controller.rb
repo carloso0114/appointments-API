05ec516bfd88c25aa61bda5e0e169778d0acafd1
pountzas <nikos@pountzas.gr>
2021-10-25 23:22:29 +0300

    create add and remove methods API

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,7 +1,29 @@
 class CarsController < ApplicationController
-  def show; end
+  def index
+    @cars = Car.all
+    render json: { cars: @cars }
+  end
 
-  def new; end
+  def create
+    @car = Car.new(car_params)
+    if @car.save
+      render json: { message: 'Car successfully created' }, status: 201
+    else
+      render json: { message: 'Unable to create car' }, status: 400
+    end
+  end
 
-  def create; end
+  def destroy
+    @car = Car.find(params[:id])
+    if @car.destroy
+      render json: { message: 'Car successfully deleted' }, status: 200
+    else
+      render json: { message: 'Unable to delete car' }, status: 400
+    end
+  end
+
+  private
+  def car_params
+    params.require(:car).permit(:name, :model, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   get 'cars/new'
   get 'cars/create'
   get '/signup', to: 'users#new'
+
+  resources :cars
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
