72f82a6bc7faee758899db11f478e92d5d67601b
Carloso0114 <carlossospinag@gmail.com>
2021-10-25 09:36:52 -0500

    fix linters errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
-class UsersController < ApplicationController
-  def new
-  end
+# frozen_string_literal: true
 
-  def create
-  end
+class UsersController < ApplicationController
+  def new; end
+
+  def create; end
 end
