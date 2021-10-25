57d020ce55299a7edd7d408bc34d3ae34818ca93
pountzas <nikos@pountzas.gr>
2021-10-26 01:04:38 +0300

    create endpoints for reservations

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'reservations/new'
-  get 'reservations/create'
-  get 'reservations/show'
-
   get '/users', to: 'users#index'
+  
   post '/signup', to: 'users#create'
+  
   resources :cars
+  resources :reservations
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
