b2c0c923da1917e322a874f6679dea4aec0c1982
Carloso0114 <carlossospinag@gmail.com>
2021-10-25 17:39:17 -0500

    add cloudinary gem, and update validations on cars model

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'hostingfreeplease'
+  config.api_key = '326943284726431'
+  config.api_secret = 'p0oxs_wFBk3-GhSMb6t13YdX4Bo'
+  config.secure = true
+  config.cdn_subdomain = true
+end
