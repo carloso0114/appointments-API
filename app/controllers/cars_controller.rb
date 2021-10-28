741cdfb0336529302ad2ca5f1ab338d3c9b5dfa1
Carloso0114 <carlossospinag@gmail.com>
2021-10-28 12:28:32 -0500

    update cars_controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,31 +1,26 @@
 class CarsController < ApplicationController
+  before_action :create_car, only: [:create]
   def index
     @cars = Car.all
     render json: { cars: @cars }
   end
 
   def create
-    @car = Car.new(car_params)
-    @car.attach_image
     if @car.save
       render json: { message: 'Car successfully created' }, status: 201
     else
       render json: { message: 'Unable to create car' }, status: 400
     end
   end
-
-  def destroy
-    @car = Car.find(params[:id])
-    if @car.destroy
-      render json: { message: 'Car successfully deleted' }, status: 200
-    else
-      render json: { message: 'Unable to delete car' }, status: 400
-    end
-  end
+end
 
   private
 
-  def car_params
-    params.require(:car).permit(:name, :model, :description, :image)
-  end
+def create_car
+  @car = Car.new(car_params)
+  @car.attach_image
+end
+
+def car_params
+  params.require(:car).permit(:name, :model, :description, :image)
 end
